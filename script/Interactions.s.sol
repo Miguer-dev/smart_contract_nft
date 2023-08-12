da146a526287114a1a8c47bc2e8207c6e31a4760
Miguel Martinez <miguerdev@gmail.com>
2023-08-12 02:32:56 -0400

    Update Interactions contract, add mint star

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,9 +5,11 @@ pragma solidity >=0.8.0 <0.9.0;
 import {Script} from "forge-std/Script.sol";
 import {DeployBasicNft} from "./DeployBasicNft.s.sol";
 import {BasicNft} from "../../src/BasicNft.sol";
+import {DeployStarNft} from "./DeployStarNft.s.sol";
+import {StarNft} from "../../src/StarNft.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
-contract MintNft is Script {
+contract MintPuppyNft is Script {
     string private constant TOKEN_URI =
         "ipfs://bafybeicpba3h4herwwg2ofcbovzmez4nyilb42ct3oiiox7y25dxhlfx44/?filename=shiba-inu.json";
 
@@ -19,6 +21,19 @@ contract MintNft is Script {
     function mintNftOnContract(address mostRecentlyDeployed) public {
         vm.startBroadcast();
         BasicNft(mostRecentlyDeployed).mintNft(TOKEN_URI);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintStarNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("StarNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        StarNft(mostRecentlyDeployed).mintNft();
         vm.stopBroadcast();
     }
 }
